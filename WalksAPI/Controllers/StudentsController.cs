180ddc934d21a5617e4df7fdb94d07578599669f
MsN <mohsen.s7s.ab@gmail.com>
2023-08-14 15:40:02 +0330

    GetAllStudents API

---
+++ WalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace WalksAPI.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "mmd", "ali", "jafar" };
+            return Ok(studentNames);
+        }
+    }
+}
