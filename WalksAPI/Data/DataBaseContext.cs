6a7b4b25ab2a018778bc28cefcd9b170018dee8b
MsN <mohsen.s7s.ab@gmail.com>
2023-08-19 23:00:34 +0330

    Add dbcontext and migration

---
+++ WalksAPI/Data/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WalksAPI.Models.Domain;
+
+namespace WalksAPI.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options): base(options)
+        {
+            
+        }
+        public DbSet<Difficulty> difficulties { get; set; }
+        public DbSet<Region> regions { get; set; }
+        public DbSet<Walk> walks { get; set; }
+    }
+}
---
+++ WalksAPI/Migrations/20230819192712_Init.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WalksAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "difficulties",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_difficulties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "regions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RegionImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "walks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LengthInKm = table.Column<double>(type: "float", nullable: false),
+                    WalkImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DifficultyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RegionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_walks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_walks_difficulties_DifficultyId",
+                        column: x => x.DifficultyId,
+                        principalTable: "difficulties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_walks_regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_walks_DifficultyId",
+                table: "walks",
+                column: "DifficultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_walks_RegionId",
+                table: "walks",
+                column: "RegionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "walks");
+
+            migrationBuilder.DropTable(
+                name: "difficulties");
+
+            migrationBuilder.DropTable(
+                name: "regions");
+        }
+    }
+}
