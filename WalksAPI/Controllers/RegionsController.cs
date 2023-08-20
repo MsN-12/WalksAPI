43f723e851fa3829578481c4b221edd7cab48df0
MsN <mohsen.s7s.ab@gmail.com>
2023-08-20 13:56:38 +0330

    GetAll and Get Api

---
+++ WalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WalksAPI.Models.Domain;
+
+namespace WalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var region = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Lamerd Region",
+                    Code = "Lmd",
+                    RegionImgUrl = "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Shiraz Region",
+                    Code = "Shz",
+                    RegionImgUrl = "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                }
+            };
+            return Ok(region);
+        }
+    }
+}
